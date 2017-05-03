f009f0778eb93522cb5ac0b32fd42e801b5b9dfe
mvasquez <mvasquez@mavem.com>
2017-05-02 17:33:57 -0700

    Add granular exceptions to ESLint rules for build scripts where you don't care if logging to the console

--- buildScripts/startMessage.js
+++ buildScripts/startMessage.js
@@ -4,5 +4,5 @@
 // ES6 (dependent upon Babel)
 import chalk from "chalk";
 
-console.log(chalk.green("Starting app in dev mode..."));
+console.log(chalk.green("Starting app in dev mode..."));    // eslint-disable-line no-console
 
